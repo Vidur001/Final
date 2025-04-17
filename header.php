b144b5eee7f033b8c81607ae3d5a791d15dbd71d
Vidur001 <vidursingh001@algomau.ca>
2025-04-17 15:25:17 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Final Exam</title>
-    <link rel="icon" href="https://www.algomau.ca/wp-content/themes/algoma/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="https://umcollege.ca/wp-content/uploads/2022/11/Algoma-University-Logo.png" type="image/x-icon">
 </head>
 <body>
